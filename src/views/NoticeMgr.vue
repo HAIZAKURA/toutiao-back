46aa98fbe3085aaf301aa759b67e87ffc4a27484
HAIZAKURA <hhbilly99@gmail.com>
2020-06-21 22:29:17 +0800

    create NoticeMgr.vue

---
+++ src/views/NoticeMgr.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notice-mgr">
+    <div class="title">
+      <span class="title-text">通&nbsp;知&nbsp;管&nbsp;理</span>
+    </div>
+    <el-divider></el-divider>
+    <span>Notice Management</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NoticeMgr"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    text-align left
+    &-text
+      font-size 2em
+</style>

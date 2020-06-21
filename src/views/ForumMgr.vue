cdd1add5ddfe8cf707efa7a8e71e9c0b4bdc800f
HAIZAKURA <hhbilly99@gmail.com>
2020-06-21 22:29:10 +0800

    create ForumMgr.vue

---
+++ src/views/ForumMgr.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="forum-mgr">
+    <div class="title">
+      <span class="title-text">分&nbsp;区&nbsp;管&nbsp;理</span>
+    </div>
+    <el-divider></el-divider>
+    <span>Forum Management</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ForumMgr"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    text-align left
+    &-text
+      font-size 2em
+</style>

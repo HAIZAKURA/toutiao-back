6be1bef1e96e71de987317b7e8cc59427ce4c06e
HAIZAKURA <hhbilly99@gmail.com>
2020-06-22 12:48:26 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,9 +9,16 @@
       <a href="https://nya.run" target="_blank">
         <img src="https://img.shields.io/badge/Author-HAIZAKURA-b68469?style=flat-square" alt="HAIZAKURA" />
       </a>
-      <a href="" target="_blank">
+      <br />
+      <a href="https://github.com/HAIZAKURA/toutiao-back/blob/master/LICENSE" target="_blank">
         <img src="https://img.shields.io/github/license/HAIZAKURA/toutiao-back?style=flat-square" alt="License" />
       </a>
+      <br />
+      <a href="https://www.codacy.com/manual/HAIZAKURA/toutiao-back?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=HAIZAKURA/toutiao-back&amp;utm_campaign=Badge_Grade">
+        <img src="https://app.codacy.com/project/badge/Grade/79f4fb6f7ff742b2bf7acb59dba96496" alt="codacy"/>
+      </a>
+      <el-divider></el-divider>
+      <span>HAIZAKURA&nbsp;&copy; &nbsp;2020</span>
     </div>
   </div>
 </template>
